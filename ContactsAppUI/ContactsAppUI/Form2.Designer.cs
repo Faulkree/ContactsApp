198f518fe2b7883786e3b2413f1588e68f2a2a88
Surgutstaff <Surgutstaff@mail.ru>
2019-01-05 23:17:07 +0700

    заработай 2

--- ContactsAppUI/ContactsAppUI/Form2.Designer.cs
+++ ContactsAppUI/ContactsAppUI/Form2.Designer.cs
@@ -28,179 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.SecondName = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.Birth = new System.Windows.Forms.Label();
+            this.Phone = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.BirthTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SecondNameTextBox1 = new System.Windows.Forms.TextBox();
             this.NameTextBox1 = new System.Windows.Forms.TextBox();
             this.PhoneTextBox1 = new System.Windows.Forms.TextBox();
             this.EmailtextBox1 = new System.Windows.Forms.TextBox();
             this.VKtextBox1 = new System.Windows.Forms.TextBox();
+            this.BirthTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SecondNameTextBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // SecondName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "SecondName:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.SecondName.AutoSize = true;
+            this.SecondName.Location = new System.Drawing.Point(12, 15);
+            this.SecondName.Name = "SecondName";
+            this.SecondName.Size = new System.Drawing.Size(72, 13);
+            this.SecondName.TabIndex = 0;
+            this.SecondName.Text = "SecondName";
+            this.SecondName.Click += new System.EventHandler(this.SecondName_Click);
             // 
-            // label2
+            // Name
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Name:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(49, 46);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(35, 13);
+            this.Name.TabIndex = 1;
+            this.Name.Text = "Name";
             // 
-            // label3
+            // Birth
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 112);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Birthday:";
+            this.Birth.AutoSize = true;
+            this.Birth.Location = new System.Drawing.Point(56, 74);
+            this.Birth.Name = "Birth";
+            this.Birth.Size = new System.Drawing.Size(28, 13);
+            this.Birth.TabIndex = 2;
+            this.Birth.Text = "Birth";
             // 
-            // label4
+            // Phone
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 147);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Phone";
+            this.Phone.AutoSize = true;
+            this.Phone.Location = new System.Drawing.Point(49, 109);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(38, 13);
+            this.Phone.TabIndex = 3;
+            this.Phone.Text = "Phone";
+            this.Phone.Click += new System.EventHandler(this.Phone_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 184);
+            this.label5.Location = new System.Drawing.Point(49, 135);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(32, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "E-mail";
+            this.label5.Text = "Email";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(33, 222);
+            this.label6.Location = new System.Drawing.Point(49, 165);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.Size = new System.Drawing.Size(32, 13);
             this.label6.TabIndex = 5;
-            this.label6.Text = "vk.com:";
-            // 
-            // BirthTimePicker1
-            // 
-            this.BirthTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.BirthTimePicker1.Location = new System.Drawing.Point(87, 112);
-            this.BirthTimePicker1.Name = "BirthTimePicker1";
-            this.BirthTimePicker1.Size = new System.Drawing.Size(138, 20);
-            this.BirthTimePicker1.TabIndex = 11;
-            this.BirthTimePicker1.ValueChanged += new System.EventHandler(this.BirthTimePicker1_ValueChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(312, 264);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(394, 264);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // SecondNameTextBox1
-            // 
-            this.SecondNameTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.SecondNameTextBox1.Location = new System.Drawing.Point(88, 42);
-            this.SecondNameTextBox1.Name = "SecondNameTextBox1";
-            this.SecondNameTextBox1.Size = new System.Drawing.Size(227, 20);
-            this.SecondNameTextBox1.TabIndex = 14;
-            this.SecondNameTextBox1.TextChanged += new System.EventHandler(this.SecondNameTextBox1_TextChanged);
+            this.label6.Text = "IDVK";
             // 
             // NameTextBox1
             // 
-            this.NameTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.NameTextBox1.Location = new System.Drawing.Point(87, 74);
+            this.NameTextBox1.Location = new System.Drawing.Point(90, 43);
             this.NameTextBox1.Name = "NameTextBox1";
-            this.NameTextBox1.Size = new System.Drawing.Size(228, 20);
-            this.NameTextBox1.TabIndex = 15;
-            this.NameTextBox1.TextChanged += new System.EventHandler(this.NameTextBox1_TextChanged);
+            this.NameTextBox1.Size = new System.Drawing.Size(200, 20);
+            this.NameTextBox1.TabIndex = 7;
+            this.NameTextBox1.TextChanged += new System.EventHandler(this.NameTextBox1_TextChanged_1);
             // 
             // PhoneTextBox1
             // 
-            this.PhoneTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.PhoneTextBox1.Location = new System.Drawing.Point(87, 147);
+            this.PhoneTextBox1.Location = new System.Drawing.Point(90, 106);
             this.PhoneTextBox1.Name = "PhoneTextBox1";
-            this.PhoneTextBox1.Size = new System.Drawing.Size(228, 20);
-            this.PhoneTextBox1.TabIndex = 16;
-            this.PhoneTextBox1.TextChanged += new System.EventHandler(this.PhoneTextBox1_TextChanged);
+            this.PhoneTextBox1.Size = new System.Drawing.Size(200, 20);
+            this.PhoneTextBox1.TabIndex = 8;
+            this.PhoneTextBox1.TextChanged += new System.EventHandler(this.PhoneTextBox1_TextChanged_1);
             // 
             // EmailtextBox1
             // 
-            this.EmailtextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.EmailtextBox1.Location = new System.Drawing.Point(88, 181);
+            this.EmailtextBox1.Location = new System.Drawing.Point(90, 132);
             this.EmailtextBox1.Name = "EmailtextBox1";
-            this.EmailtextBox1.Size = new System.Drawing.Size(227, 20);
-            this.EmailtextBox1.TabIndex = 17;
-            this.EmailtextBox1.TextChanged += new System.EventHandler(this.EmailtextBox1_TextChanged);
+            this.EmailtextBox1.Size = new System.Drawing.Size(200, 20);
+            this.EmailtextBox1.TabIndex = 9;
+            this.EmailtextBox1.TextChanged += new System.EventHandler(this.EmailtextBox1_TextChanged_1);
             // 
             // VKtextBox1
             // 
-            this.VKtextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.VKtextBox1.Location = new System.Drawing.Point(88, 222);
+            this.VKtextBox1.Location = new System.Drawing.Point(90, 158);
             this.VKtextBox1.Name = "VKtextBox1";
-            this.VKtextBox1.Size = new System.Drawing.Size(227, 20);
-            this.VKtextBox1.TabIndex = 18;
-            this.VKtextBox1.TextChanged += new System.EventHandler(this.VKtextBox1_TextChanged);
+            this.VKtextBox1.Size = new System.Drawing.Size(200, 20);
+            this.VKtextBox1.TabIndex = 10;
+            this.VKtextBox1.TextChanged += new System.EventHandler(this.VKtextBox1_TextChanged_1);
+            // 
+            // BirthTimePicker1
+            // 
+            this.BirthTimePicker1.Location = new System.Drawing.Point(90, 74);
+            this.BirthTimePicker1.Name = "BirthTimePicker1";
+            this.BirthTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.BirthTimePicker1.TabIndex = 11;
+            this.BirthTimePicker1.ValueChanged += new System.EventHandler(this.BirthTimePicker1_ValueChanged_1);
+            // 
+            // SecondNameTextBox1
+            // 
+            this.SecondNameTextBox1.Location = new System.Drawing.Point(90, 12);
+            this.SecondNameTextBox1.Name = "SecondNameTextBox1";
+            this.SecondNameTextBox1.Size = new System.Drawing.Size(200, 20);
+            this.SecondNameTextBox1.TabIndex = 12;
+            this.SecondNameTextBox1.TextChanged += new System.EventHandler(this.SecondNameTextBox1_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 303);
+            this.ClientSize = new System.Drawing.Size(352, 243);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SecondNameTextBox1);
+            this.Controls.Add(this.BirthTimePicker1);
             this.Controls.Add(this.VKtextBox1);
             this.Controls.Add(this.EmailtextBox1);
             this.Controls.Add(this.PhoneTextBox1);
             this.Controls.Add(this.NameTextBox1);
-            this.Controls.Add(this.SecondNameTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.BirthTimePicker1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.Birth);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.SecondName);
             this.Name = "Form2";
-            this.Text = "Add/Edit Contact";
+            this.Text = "Add/Edit";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -209,19 +197,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label SecondName;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label Birth;
+        private System.Windows.Forms.Label Phone;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.DateTimePicker BirthTimePicker1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox SecondNameTextBox1;
         private System.Windows.Forms.TextBox NameTextBox1;
         private System.Windows.Forms.TextBox PhoneTextBox1;
         private System.Windows.Forms.TextBox EmailtextBox1;
         private System.Windows.Forms.TextBox VKtextBox1;
+        private System.Windows.Forms.DateTimePicker BirthTimePicker1;
+        private System.Windows.Forms.TextBox SecondNameTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ContactsAppUI/ContactsAppUI/Form2.cs
+++ ContactsAppUI/ContactsAppUI/Form2.cs
@@ -14,62 +14,17 @@ namespace ContactsAppUI
     public partial class Form2 : Form
     {
         private Contact _contactsplus = new Contact();
-   
+
         private PhoneNumber _phone = new PhoneNumber();
 
         public Form2()
         {
             InitializeComponent();
             BirthTimePicker1.MaxDate = DateTime.Now;
-           
+
         }
 
         
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            bool flag;
-            try
-            {
-               
-               flag = true;
-                _phone.Number = System.Int64.Parse(PhoneTextBox1.Text);
-                _contactsplus.SecondName = SecondNameTextBox1.Text;
-                _contactsplus.Name = NameTextBox1.Text;
-                _contactsplus.Birth = BirthTimePicker1.Value;
-                _contactsplus.Phone = _phone;
-                _contactsplus.Email = EmailtextBox1.Text;
-                _contactsplus.IDVk = VKtextBox1.Text;
-                _data.TxtBox = _contactsplus.SecondName;
-                _data._contactsplus = _contactsplus;
-               
-
-            }
-
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Неверный ввод данных");
-                flag = false;
-            }
-
-                if (flag == true)
-            {
-                this.Close();
-                DialogResult = DialogResult.OK;
-               
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Form1 main = this.Owner as Form1;
-            var form1 = new Form1();
-            if (main != null)
-            {
-                Data = null;
-            }
-            this.Close();
-        }
 
         private void Form2_Load(object sender, EventArgs e)
         {
@@ -82,7 +37,7 @@ namespace ContactsAppUI
                 BirthTimePicker1.Value = Data._contactsplus.Birth;
                 PhoneTextBox1.Text = Convert.ToString(Data._contactsplus.Phone.Number);
             }
-            
+
         }
         public class DataMainForm
         {
@@ -101,27 +56,81 @@ namespace ContactsAppUI
                 _data = value;
             }
         }
-     
+
 
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void SecondNameTextBox1_TextChanged(object sender, EventArgs e)
+      
+
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            bool flag;
+            try
+            {
+
+                flag = true;
+                _phone.Number = System.Int64.Parse(PhoneTextBox1.Text);
+                _contactsplus.SecondName = SecondNameTextBox1.Text;
+                _contactsplus.Name = NameTextBox1.Text;
+                _contactsplus.Birth = BirthTimePicker1.Value;
+                _contactsplus.Phone = _phone;
+                _contactsplus.Email = EmailtextBox1.Text;
+                _contactsplus.IDVk = VKtextBox1.Text;
+                _data.TxtBox = _contactsplus.SecondName;
+                _data._contactsplus = _contactsplus;
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Неверный ввод данных");
+                flag = false;
+            }
+
+            if (flag == true)
+            {
+                this.Close();
+                DialogResult = DialogResult.OK;
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Form1 main = this.Owner as Form1;
+            var form1 = new Form1();
+            if (main != null)
+            {
+                Data = null;
+            }
+            this.Close();
+        }
+
+        private void SecondNameTextBox1_TextChanged_1(object sender, EventArgs e)
         {
             try
             {
                 _contactsplus.SecondName = SecondNameTextBox1.Text;
                 SecondNameTextBox1.BackColor = Color.White;
             }
-            catch(Exception)
+            catch (Exception)
             {
                 SecondNameTextBox1.BackColor = Color.LightSalmon;
             }
+
         }
 
-        private void NameTextBox1_TextChanged(object sender, EventArgs e)
+        private void NameTextBox1_TextChanged_1(object sender, EventArgs e)
         {
             try
             {
@@ -134,32 +143,12 @@ namespace ContactsAppUI
             }
         }
 
-        private void BirthTimePicker1_ValueChanged(object sender, EventArgs e)
-       {
-           
-            
-                try
-                {
-                    _contactsplus.Birth = BirthTimePicker1.Value;
-                    BirthTimePicker1.BackColor = Color.White;
-                }
-                catch (Exception )
-                {
-                    BirthTimePicker1.BackColor = Color.LightSalmon;
-                  
-                }
-            
-            
-             
-
-        }
-
-        private void PhoneTextBox1_TextChanged(object sender, EventArgs e)
+        private void PhoneTextBox1_TextChanged_1(object sender, EventArgs e)
         {
-           
+
         }
 
-        private void EmailtextBox1_TextChanged(object sender, EventArgs e)
+        private void EmailtextBox1_TextChanged_1(object sender, EventArgs e)
         {
             try
             {
@@ -173,11 +162,11 @@ namespace ContactsAppUI
             }
         }
 
-        private void VKtextBox1_TextChanged(object sender, EventArgs e)
+        private void VKtextBox1_TextChanged_1(object sender, EventArgs e)
         {
             try
             {
-              
+
                 _contactsplus.IDVk = VKtextBox1.Text;
                 VKtextBox1.BackColor = Color.White;
             }
@@ -187,7 +176,26 @@ namespace ContactsAppUI
             }
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void BirthTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+            try
+            {
+                _contactsplus.Birth = BirthTimePicker1.Value;
+                BirthTimePicker1.BackColor = Color.White;
+            }
+            catch (Exception)
+            {
+                BirthTimePicker1.BackColor = Color.LightSalmon;
+
+            }
+        }
+
+        private void SecondName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Phone_Click(object sender, EventArgs e)
         {
 
         }
